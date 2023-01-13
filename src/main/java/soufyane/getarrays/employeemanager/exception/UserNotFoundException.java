6d273e62182b1cfa4b39d970826447966357d02b
Soufyane-RAHHAL <soufyane.rahhal@gmail.com>
2023-01-14 00:10:46 +0100

    add service

---
+++ src/main/java/soufyane/getarrays/employeemanager/exception/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package soufyane.getarrays.employeemanager.exception;
+
+public class UserNotFoundException extends RuntimeException{
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
